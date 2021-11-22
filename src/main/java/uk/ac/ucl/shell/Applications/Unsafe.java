d48a68d7a47fa245a5b906a5b1010b28a98e9093
Huijie Yan <zcabhya@ucl.ac.uk>
2021-11-22 22:21:22 +0000

    unsafe for testing

---
+++ src/main/java/uk/ac/ucl/shell/Applications/Unsafe.java
@@ -0,0 +1,22 @@
+package uk.ac.ucl.shell.Applications;
+
+import uk.ac.ucl.shell.ShellApplication;
+
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+public class Unsafe implements ShellApplication {
+    private ShellApplication app;
+
+    public Unsafe(ShellApplication app){
+        this.app = app;
+    }
+    @Override
+    public String exec(List<String> appArgs) throws RuntimeException {
+        try {
+            return this.app.exec(appArgs);
+        }catch (RuntimeException e){
+            throw new RuntimeException("ignore" + e.getMessage());
+        }
+    }
+}
